e576854b0e1c2a69de18060838e18afb4defa0a5
Mark Rego <rego121@gmail.com>
2019-08-07 16:23:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,41 @@
 def create_an_empty_array
-  
+  newarr = []
+  return newarr
 end
 
 def create_an_array
-  
+newarr = ["one", 'two', 'three']
+return newarr
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = []
+array << element
+return array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
